01d35de7517ce4eb269f9f71305c91a6bfe96618
jake <jakelynam2005@outlook.com>
2026-08-27 17:54:59 +0100

    created client and tested(worked)

---
+++ client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://localhost:9090") //makes request to server and gets resp/error
+	if err != nil {
+		fmt.Println("error making request:", err)
+		return
+	}
+	defer resp.Body.Close() // closes stream after getting response
+
+	body, err := io.ReadAll(resp.Body) //reads response to allow for manipulation in program
+	if err != nil {
+		fmt.Println("error reading body:", err)
+		return
+	}
+
+	fmt.Println("Status:", resp.Status)    //checks the status on the request
+	fmt.Println("Response:", string(body)) //prints the response to terminal
+
+}
